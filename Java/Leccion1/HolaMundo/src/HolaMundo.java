290d3ad4826e7f9dd507d40f56e7840462524f86
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-04-19 21:09:37 -0300

    Hola mundo en Java - Leccion 1

---
+++ Java/Leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,22 @@
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java");
+        
+        //Variable tipo int
+        int miVariable = 10;
+        System.out.println(miVariable);
+        
+        //renombrar variable
+        miVariable = 25;
+        System.out.println(miVariable);
+        
+        //Variable tipo string
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+        //Apretando control + click izq sobre la variable impresa nos envía a donde fue definida la variable
+        
+    }
+}
