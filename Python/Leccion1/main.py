4a50100b7ef691b6867128dd8b0368dc461c3eb2
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-04-19 21:10:23 -0300

    Hola mundo en Python - Leccion 1

---
+++ Python/Leccion1/main.py
@@ -0,0 +1,27 @@
+miVariable = 3
+print(miVariable)
+# dato tipo int
+
+miVariable = "Hola a todos los estudiantes de la tecnicatura"
+print(miVariable)
+# dato tipo string
+
+miVariable = 3.5
+# dato tipo float
+print(miVariable)
+
+x = 10
+y = 2
+z = x + y
+print(id(x))
+
+#Las literales se escriben de la siguiente manera x___
+#X seguido de los ultimos 3 digitos de la memoria: ejemplo = "x104"
+#Las literales cambian constantemente, el id no es estático
+
+""" print(id(x))
+print(id(y))
+print(id(z)) """
+
+#En este ejemplo x=248 y=992 z=312, si se hace otra vez sus valores cambian
+# A esto se lo conoce como referencia de memoria o simplemente referencia
