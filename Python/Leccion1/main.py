937d85d131386b3565b42350baa3188a46067c90
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-05-02 15:12:59 -0300

    Clase 2 - Variables en Python

--- Python/Leccion1/main.py
+++ Python/Leccion1/main.py
@@ -1,27 +1,45 @@
-miVariable = 3
-print(miVariable)
+#  Clase 1  #
+
+"""
+print('Hola Mundo')
+"""
+
+#  Clase 2  #
+
+# Variables en Python
+
+"""
+# ¡¡ Una literal es un valor que podemos asignar a nuestras variables !!
+
+# miVariable = 3
+# print(miVariable)
 # dato tipo int
 
-miVariable = "Hola a todos los estudiantes de la tecnicatura"
-print(miVariable)
+# miVariable = "Hola a todos los estudiantes de la tecnicatura"
+# print(miVariable)
 # dato tipo string
 
-miVariable = 3.5
+# miVariable = 3.5
 # dato tipo float
-print(miVariable)
+# print(miVariable)
 
 x = 10
 y = 2
 z = x + y
+# print(id(x))
+
+# Las literales se escriben de la siguiente manera x___
+# X seguido de los ultimos 3 digitos de la memoria: ejemplo = "x104"
+# Las literales cambian constantemente, el id no es estático
+
 print(id(x))
-
-#Las literales se escriben de la siguiente manera x___
-#X seguido de los ultimos 3 digitos de la memoria: ejemplo = "x104"
-#Las literales cambian constantemente, el id no es estático
-
-""" print(id(x))
 print(id(y))
-print(id(z)) """
+print(id(z))
 
-#En este ejemplo x=248 y=992 z=312, si se hace otra vez sus valores cambian
+# En este ejemplo x=248 y=992 z=312, si se hace otra vez sus valores cambian
 # A esto se lo conoce como referencia de memoria o simplemente referencia
+
+
+"""
+
+
