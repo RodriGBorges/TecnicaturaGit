d98de0181f8a3d0b185c1977feeca9f2cc915de7
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-09-29 23:12:14 -0300

    Clase 3 JavaScript - Tipos de Datos
    GitHub Desktop

---
+++ JavaScript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,19 @@
+// Tipos de Datos en JavaScript
+/* 
+La sintaxis en lo que es comentarios es muy similar 
+a la de Java realmente diriamos que es indentica
+*/
+
+var nombre = "Ariel"; // Tipo String
+console.log(nombre);
+
+var numero = 3000; // Tipo String
+console.log(numero);
+
+var objeto = {
+    nombre: "Ariel",
+    apellido: 'Betancud',
+    telefono: '2614567893'
+}
+
+console.log(objeto);
