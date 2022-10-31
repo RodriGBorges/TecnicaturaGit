ed085b20aea922ca2048695ece726904edd36283
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-10-31 14:25:51 -0300

    Clase 7 JavaScript - Similitudes parte 2

---
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,16 @@
+//Ejercicio para encontrar números pares
+let parInpar = 10;
+
+if(parInpar % 2 == 0) {
+    console.log('Es PAR');
+} else {
+    console.log('Es IMPAR');
+}
+
+//Ejercicio mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto) {
+    console.log('Es mayor de edad');
+} else {
+    console.log('No es mayor de edad');
+}
