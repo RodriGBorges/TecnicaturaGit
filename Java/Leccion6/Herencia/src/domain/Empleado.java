f212c165a023f036e96dfc4b941b471fc2b4f50d
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-10-31 18:31:18 -0300

    Clase 9 Java - Herencia

---
+++ Java/Leccion6/Herencia/src/domain/Empleado.java
@@ -0,0 +1,43 @@
+
+package domain;
+
+// Empleado extends Persona => Empleado hija de Persona
+public class Empleado extends Persona {
+    
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados; //Es para incrementar
+    
+    //Constructor
+    public Empleado(String nombre, double sueldo) {
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    //Getters and setters
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+    //toString con StringBuilder
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
---
+++ Java/Leccion6/Herencia/src/test/TestHerencia.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Liliana", 87000.0);
+        System.out.println("empleado1 = " + empleado1);
+        
+        Date fecha1 = new Date();
+        
+        Cliente cliente1 = new Cliente(fecha1, true, "Lili", 'F', 37, "24 de octubre 321");
+        System.out.println("cliente1 = " + cliente1);
+    }
+}
