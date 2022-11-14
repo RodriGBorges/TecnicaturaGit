19f9299aa375a6dd6ec8d2138c16a3a2ab744357
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2022-11-14 15:35:02 -0300

    Clase 9 JavaScript - Mas operadores y funciones

--- JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -23,3 +23,45 @@ if(dentroRango >= valorMin && dentroRango <= valorMax) {
 } else {
     console.log('No está dentro del rango');
 }
+
+//ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = false, diaDescanso = true;
+if(vacaciones || diaDescanso) {
+    console.log('El padre puede asistir');
+} else {
+    console.log('El padre no puede asistir');
+}
+
+//Operador ternario
+
+let resultado2 = 3 > 2 ? 'Verdadero' : 'Falso';
+console.log(resultado2);
+
+let numero = 8;
+resultado2 = numero % 2 == 0 ? 'Es PAR' : 'Es IMPAR';
+console.log(resultado2);
+
+//Convertir String a Number
+
+let miNumero = '18x'; //Es un string
+console.log(typeof miNumero);
+
+let edad2 = Number(miNumero); // Number() es una función
+console.log(typeof edad2); // Es un number
+console.log(edad2);
+
+//isNaN() => no es un número / is not a number (devuelve un resultado booleano)
+if(isNaN(edad2)) {
+    console.log('La variable no contiene solo números');
+} else {
+    if(edad2 >= 18) {
+        console.log('Puede votar');
+    } else {
+        console.log('No puede votar');
+    }
+}
+
+let resultado3 = edad2 >= 18 ? 'Puede votar' : 'No puede votar';
+console.log(resultado3);
+
+//Función isNaN
