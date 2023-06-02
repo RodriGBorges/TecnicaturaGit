c695edd6c647a74e61ecd4fe2657641c72c17c05
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-06-01 22:14:59 -0300

    Clase 08 JavaScript - Static

--- JavaScript2/Leccion05/05-03-clases.js
+++ JavaScript2/Leccion05/05-03-clases.js
@@ -2,9 +2,23 @@
 
 // let persona3 = new Persona('Carla', 'Ponce'); No se permite antes de crear la clase
 class Persona { //Clase padre
+
+    static contadorPersonas = 0; //Atributo estático
+    email = 'Valor default email'; //Atributo no estático
+
+    static get MAX_OBJ() { // Este método simula una constante
+        return 5;
+    }
+
     constructor(nombre, apellido) {
         this._nombre = nombre,
         this._apellido = apellido
+        if(Persona.contadorPersonas < Persona.MAX_OBJ) {
+            this.idPersona = ++Persona.contadorPersonas;
+        } else {
+            console.log('Se ha superado el máximo de objetos permitidos');
+        }
+        //console.log('Se incrementa el contador: ' + Persona.contadorObjetosPersona);
     }
 
     get nombre() {
@@ -24,7 +38,7 @@ class Persona { //Clase padre
     }
 
     nombreCompleto() {
-        return this._nombre + ' ' + this._apellido;
+        return this.idPersona + ' ' + this._nombre + ' ' + this._apellido;
     }
 
     //Sobreescribiendo el método de la clase padre (Object)
@@ -32,6 +46,14 @@ class Persona { //Clase padre
         //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecución
         //El método que se ejecuta depende si es una referencia de tipo padre o hija
         return this.nombreCompleto();
+    }
+
+    static saludar() {
+        console.log('Saludos desde este método static');
+    }
+
+    static saludar2(persona) {
+        console.log(persona.nombre + ' ' + persona._apellido);
     }
 }
 
@@ -82,3 +104,38 @@ console.log(empleado1.nombreCompleto()); //hereda el método de la clase padre
 // Object.prototype.toString //Esta es la manera de acceder a atributos y métodos de manera dinámica
 console.log(empleado1.toString());
 console.log(persona1.toString());
+
+//persona1.saludar(); no se utiliza desde el objeto
+
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email); No se puede acceder desde la clase
+
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+
+let persona3 = new Persona('Carla', 'Gimenez');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
+
+console.log(Persona.MAX_OBJ);
+//Persona.MAX_OBJ = 10; No se puede modificar ni alterar
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona('Franco', 'Diaz');
+console.log(persona4.toString());
+
+let persona5 = new Persona('Liliana', 'Paz');
+console.log(persona5.toString());
