903f0e51be44b654a0964871f38efbb127822f79
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-06-01 21:35:54 -0300

    Clase 07 JavaScript - Clases parte 2

--- JavaScript2/Leccion05/05-03-clases.js
+++ JavaScript2/Leccion05/05-03-clases.js
@@ -1,14 +1,14 @@
 //Clases en JavaScript
 
 // let persona3 = new Persona('Carla', 'Ponce'); No se permite antes de crear la clase
-class Persona {
+class Persona { //Clase padre
     constructor(nombre, apellido) {
         this._nombre = nombre,
         this._apellido = apellido
     }
 
     get nombre() {
-        return this._nombre
+        return this._nombre;
     }
 
     set nombre(nombre) {
@@ -16,15 +16,26 @@ class Persona {
     }
 
     get apellido() {
-        return this._apellido
+        return this._apellido;
     }
 
     set apellido(apellido) {
         this._apellido = apellido;
     }
+
+    nombreCompleto() {
+        return this._nombre + ' ' + this._apellido;
+    }
+
+    //Sobreescribiendo el método de la clase padre (Object)
+    toString() { //Regresa un String
+        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecución
+        //El método que se ejecuta depende si es una referencia de tipo padre o hija
+        return this.nombreCompleto();
+    }
 }
 
-class Empleado extends Persona {
+class Empleado extends Persona { //Clase hija
     constructor(nombre, apellido, departamento) {
         super(nombre, apellido);
         this._departamento = departamento;
@@ -36,6 +47,11 @@ class Empleado extends Persona {
 
     set departamento(departamento) {
         this._departamento = departamento;
+    }
+
+    //Sobreescritura 
+    nombreCompleto() {
+        return super.nombreCompleto() + ', ' + this._departamento;
     }
 }
 
@@ -61,4 +77,8 @@ console.log(persona2.apellido);
 
 let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
 console.log(empleado1);
-console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto()); //hereda el método de la clase padre
+
+// Object.prototype.toString //Esta es la manera de acceder a atributos y métodos de manera dinámica
+console.log(empleado1.toString());
+console.log(persona1.toString());
